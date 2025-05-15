fa28ab50ab6ceb2bfa61ff4e102e235e7f95d84b
Zoown <andre_215@hotmail.com>
2025-05-15 22:22:25 +0200

    attempt 4

--- api/apartments.js
+++ api/apartments.js
@@ -13,10 +13,10 @@ app.use(cors());
 app.use(express.json()); // Enables JSON parsing
 
 console.log("Server is running and listening for requests... 2");
-//console.log("DB_USER:", process.env.DB_USER);
-//console.log("DB_HOST:", process.env.DB_HOST);
-//console.log("DB_NAME:", process.env.DB_NAME);
-//console.log("DB_PASS:", process.env.DB_PASS);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PASS:", process.env.DB_PASS);
 
 
 // PostgreSQL Database Connection
